b5bfeecec0f61f7103b3f6c5e8ea0083f2451953
haru <haru_1014@icloud.com>
2023-10-24 18:19:07 +0900

    add my contlloller&html&class

--- src/main/java/com/example/freestyle_training/haru/UrlInfomation.java
+++ src/main/java/com/example/freestyle_training/haru/UrlInfomation.java
@@ -1,6 +1,8 @@
 package com.example.freestyle_training.haru;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -9,10 +11,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.context.ApplicationContext;
 
+import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.servlet.ServletException;
 
 import org.springframework.ui.Model;
 
-public class haruContloller {
-    // System.out.println(UrlInfomation.name);
+@Controller
+
+public class UrlInfomation {
+    String url;
+    String name;
+    // List<String> tag = new ArrayList<String>();
 }
