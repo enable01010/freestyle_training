4af323858e043f11dc2ed79859a2d00ceb426bc8
haru <haru_1014@icloud.com>
2023-10-24 14:29:31 +0900

    new mycontllor

---
+++ src/main/java/com/example/freestyle_training/haru/haruContloller.java
@@ -0,0 +1,18 @@
+package com.example.freestyle_training.haru;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.ApplicationContext;
+
+import jakarta.servlet.ServletException;
+
+import org.springframework.ui.Model;
+
+public class haruContloller {
+
+}
