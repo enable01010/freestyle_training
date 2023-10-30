d284494e0f2f315e1a0eb424c3d800274a4f30ef
haru <haru_1014@icloud.com>
2023-10-31 03:41:14 +0900

    haruclass

--- src/main/java/com/example/freestyle_training/haru/haruContloller.java
+++ src/main/java/com/example/freestyle_training/haru/haruContloller.java
@@ -1,6 +1,7 @@
 package com.example.freestyle_training.haru;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.freestyle_training.Taniyama.Account;
 import com.example.freestyle_training.Taniyama.UrlInfomation;
 
 import org.springframework.context.ApplicationContext;
@@ -19,8 +21,10 @@ import org.springframework.ui.Model;
 @Controller
 public class haruContloller {
     @RequestMapping(path = "/addurl")
-    public String sample(Model model) throws IOException {
+    public String sample(@ModelAttribute Account account, Model model) throws IOException {
         UrlInfomation urlInfo = new UrlInfomation();
+        model.addAttribute("Account", account);
+        // urlInfo.addTag(urlInfo.tag_Str);
         return "Hirata/addurl";
     }
 
